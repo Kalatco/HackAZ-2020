5611276de9deeded6bd2213bf6149757dbd13502
Kalatco <AndrewRaftovich@gmail.com>
2020-01-19 05:52:00 -0700

    added support for API call

--- Planet.js
+++ Planet.js
@@ -1,7 +1,11 @@
 class Planet {
-    constructor(x, y, radius){
+    constructor(x, y, radius, goal, color, title, hours){
         this.x = x;
         this.y = y;
         this.radius = radius;
+        this.goal = goal;
+        this.color = color;
+        this.title = title;
+        this.hours = hours;
     }
 }
