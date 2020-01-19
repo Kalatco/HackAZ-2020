6591e986a7dae7cf95a7ebd1eb09990d8f50d57e
Reza Munoz-Asayesh <54204263+RMA-source@users.noreply.github.com>
2020-01-18 22:40:24 -0700

    finished server

--- server.js
+++ server.js
@@ -4,18 +4,34 @@
 
 const express = require('express');
 const app = express();
+app.listen(3000, () => console.log('Server started listening at 3000'));
+
 const fs = require('fs')
 
 app.use(express.static('public'));
 
-console.log('Server started');
-app.get('/',function (req, res) {
+app.get('/', function (req, res) {
+    var collection = [];// will contain the total collection of output data
     res.header("Access-Control-Allow-Origin", "*");
-    res.send('CSV PARSER');
     var input = fs.readFileSync('test.csv');
     var data = input + '';
-    res.send(data);
+    var rows = data.split('\n').slice(1);
+
+    rows.forEach(elmnt => {
+        var row = elmnt.split(',');
+        var number = row[0];
+        var title = row[1];
+        var hours = row[2];
+        let game = [number,title,hours]; // make a sub-list for info of each game
+
+        collection.push(game)
+    });
+
+    let output = {thisArray : collection}
+
+    console.log(collection);
+
+    res.send(output);
 });
 
-app.listen(3000);
 
